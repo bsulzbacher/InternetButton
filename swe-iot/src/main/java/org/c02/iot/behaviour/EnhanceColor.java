320be9f08fa18b90c3575fa2ac73eee005e892ed
Michael Hödl <michael.hoedl@edu.campus02.at>
2016-02-06 13:10:08 +0100

    enhance color angepasst

--- swe-iot/src/main/java/org/c02/iot/behaviour/EnhanceColor.java
+++ swe-iot/src/main/java/org/c02/iot/behaviour/EnhanceColor.java
@@ -1,7 +1,32 @@
 package org.c02.iot.behaviour;
 
-public class EnhanceColor {
+import java.awt.Color;
 
-	//Test
+import org.c02.iot.InternetButtonApi;
+import org.c02.iot.InternetButtonApi.ButtonDirection;
+import org.c02.iot.cloud.api.ParticleException;
+
+public class EnhanceColor extends AbstractBehaviour {
+
+		public EnhanceColor(InternetButtonApi buttonInstance) {
+		super(buttonInstance);
+		// TODO Auto-generated constructor stub
+	}
+
+		@Override
+		public void run() {
+			int buttonCounterNorth = button.getButtonCounter(ButtonDirection.North);
+			//int buttonCounterSouth = button.getButtonCounter(ButtonDirection.South);
+			//int buttonCounterEast = button.getButtonCounter(ButtonDirection.East);
+			//int buttonCounterWest = button.getButtonCounter(ButtonDirection.West);
+			try {
+				
+				button.setLed(1, new Color(0,5,0));
+				
+			} catch (ParticleException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	
 }
