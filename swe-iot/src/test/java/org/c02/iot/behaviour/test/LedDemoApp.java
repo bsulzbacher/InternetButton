db1e00cf55d446662fd723daadaee5f8f3eef04f
julian-neumeister <julian.neumeister@edu.campus02.at>
2016-02-06 14:08:58 +0100

    LedDemoApp zu Unit-Test konvertiert

--- swe-iot/src/test/java/org/c02/iot/behaviour/test/LedDemoApp.java
+++ swe-iot/src/test/java/org/c02/iot/behaviour/test/LedDemoApp.java
@@ -8,10 +8,11 @@ import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
 import org.c02.iot.cloud.api.ParticleException;
 import org.c02.iot.cloud.examples.DemoConstants;
 import java.awt.Color;
+import org.junit.Test;
 
 public class LedDemoApp {
-	
-	public static void main(String[] args) throws IOException, ParticleException, InterruptedException {
+	@Test
+	public void TestLed() throws IOException, ParticleException, InterruptedException {
 		ParticleApiWrapper wrapper = new ParticleApiWrapperImpl(DemoConstants.deviceId, DemoConstants.accessToken);
 		InternetButtonImpl button = new InternetButtonImpl(wrapper);
 		
