f724f1f3047a1dc35ebd07e6470f95641d667fd6
Michael Hödl <michael.hoedl@edu.campus02.at>
2016-02-06 13:13:37 +0100

    Enhance Color angepasst

--- swe-iot/src/main/java/org/c02/iot/behaviour/EnhanceColor.java
+++ swe-iot/src/main/java/org/c02/iot/behaviour/EnhanceColor.java
@@ -1,6 +1,7 @@
 package org.c02.iot.behaviour;
 
 import java.awt.Color;
+import java.io.IOException;
 
 import org.c02.iot.InternetButtonApi;
 import org.c02.iot.InternetButtonApi.ButtonDirection;
@@ -15,7 +16,12 @@ public class EnhanceColor extends AbstractBehaviour {
 
 		@Override
 		public void run() {
-			int buttonCounterNorth = button.getButtonCounter(ButtonDirection.North);
+			try {
+				int buttonCounterNorth = button.getButtonCounter(ButtonDirection.North);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			//int buttonCounterSouth = button.getButtonCounter(ButtonDirection.South);
 			//int buttonCounterEast = button.getButtonCounter(ButtonDirection.East);
 			//int buttonCounterWest = button.getButtonCounter(ButtonDirection.West);
