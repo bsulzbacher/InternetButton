310a87bd41c9829d1abd636c85d98cfc078b1174
julian-neumeister <julian.neumeister@edu.campus02.at>
2016-02-06 14:43:36 +0100

    while(true) entfernt

--- swe-iot/src/main/java/org/c02/iot/behaviour/CountAndShowLed.java
+++ swe-iot/src/main/java/org/c02/iot/behaviour/CountAndShowLed.java
@@ -23,31 +23,29 @@ public class CountAndShowLed extends AbstractBehaviour {
 		int counter_east = 0;
 		int counter_west = 0;
 		
-		while (true){
-			try {
-				button.allLedsOff();
-			} catch (ParticleException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			try {
-				counter_north = button.getButtonCounter(ButtonDirection.North);
-				counter_south = button.getButtonCounter(ButtonDirection.South);
-				counter_east = button.getButtonCounter(ButtonDirection.East);
-				counter_west = button.getButtonCounter(ButtonDirection.West);
-			} catch (IOException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			
-			int counter = (counter_north + counter_south + counter_east + counter_west) % 12;
-			try {
-				button.setLed(counter, Color.green);
-			} catch (ParticleException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		try {
+			button.allLedsOff();
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			counter_north = button.getButtonCounter(ButtonDirection.North);
+			counter_south = button.getButtonCounter(ButtonDirection.South);
+			counter_east = button.getButtonCounter(ButtonDirection.East);
+			counter_west = button.getButtonCounter(ButtonDirection.West);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		int counter = (counter_north + counter_south + counter_east + counter_west) % 12;
+		try {
+			button.setLed(counter, Color.green);
+		} catch (ParticleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
