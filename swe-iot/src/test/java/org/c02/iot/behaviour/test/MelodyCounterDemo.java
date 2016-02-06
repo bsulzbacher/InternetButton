7b1fcf850210a69c8c7a7449b2117a9ff8714268
julian-neumeister <julian.neumeister@edu.campus02.at>
2016-02-06 14:40:00 +0100

    MelodyCounter zu Unit-Test konvertiert

--- swe-iot/src/test/java/org/c02/iot/behaviour/test/MelodyCounterDemo.java
+++ swe-iot/src/test/java/org/c02/iot/behaviour/test/MelodyCounterDemo.java
@@ -3,16 +3,16 @@ package org.c02.iot.behaviour.test;
 import java.io.IOException;
 
 import org.c02.iot.InternetButtonImpl;
-import org.c02.iot.InternetButtonApi.ButtonDirection;
 import org.c02.iot.cloud.api.ParticleApiWrapper;
 import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
 import org.c02.iot.cloud.api.ParticleException;
 import org.c02.iot.cloud.examples.DemoConstants;
-import java.awt.Color;
+import org.junit.Test;
 
 public class MelodyCounterDemo {
 	
-	public static void main(String[] args) throws IOException, ParticleException, InterruptedException {
+	@Test
+	public void TestMelodyCounter() throws IOException, ParticleException, InterruptedException {
 		ParticleApiWrapper wrapper = new ParticleApiWrapperImpl(DemoConstants.deviceId, DemoConstants.accessToken);
 		InternetButtonImpl button = new InternetButtonImpl(wrapper);
 		button.playMelodie();
